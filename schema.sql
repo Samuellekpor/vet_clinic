b80850bb842057a6aab728b308d45f7e960f12f6
Sammuel LEKPOR <samuellekpor@gmail.com>
2022-12-20 07:47:23 +0000

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+ CREATE TABLE animals(
+vet_clinic(# id INT GENERATED ALWAYS AS IDENTITY,
+vet_clinic(# name VARCHAR,
+vet_clinic(# date_of_birth DATE,
+vet_clinic(# escape_attempts INT,
+vet_clinic(# neutered BOOLEAN,
+vet_clinic(# weight_kg DECIMAL
+vet_clinic(# );
