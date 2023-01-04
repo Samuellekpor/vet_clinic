95b12fcfafb9de3b699b56070b25e1bceda839b3
Sammuel LEKPOR <samuellekpor@gmail.com>
2023-01-04 15:39:46 +0000

    Create indexes on foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -58,3 +58,10 @@ CREATE TABLE invoice_items(
     treatment_id INT,
     FOREIGN KEY treatment_id REFERENCES treatments(id)
 )
+
+CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+CREATE INDEX medical_history_id_asc ON treatment_historiy(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON treatment_historiy(treatment_id ASC);
+CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
+CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
