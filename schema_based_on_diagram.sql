52e427cf2dd351330d7849eea549b756183f0d85
Sammuel LEKPOR <samuellekpor@gmail.com>
2023-01-04 14:05:58 +0000

    Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,13 @@ CREATE TABLE patients(
   date_of_birth DATE
 );
 
+-- Add invoices table
+
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  FOREIGN KEY medical_history_id REFERENCES medical_histories(id)
+ );
